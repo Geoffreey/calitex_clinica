959be35bfd1d949ad485dde51dcd543c564d3f68
Geoffreey Cali <geoffrey.torres64@gmail.com>
2023-10-04 20:12:03 -0600

    Se modifica lenguage de etiquetas en archivo

--- hms/doctor/index.php
+++ hms/doctor/index.php
@@ -39,7 +39,7 @@ exit();
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Doctor Login</title>
+		<title>Iniciar sesion medico</title>
 		
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -56,17 +56,17 @@ exit();
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../../index.html">	<h2> HMS | Doctor Login</h2></a>
+				<a href="../../index.html">	<h2>CaliTex | Acceso Medicos</h2></a>
 				</div>
 
 				<div class="box-login">
 					<form class="form-login" method="post">
 						<fieldset>
 							<legend>
-								Sign in to your account
+								Iniciar sesion en su cuenta
 							</legend>
 							<p>
-								Please enter your name and password to log in.<br />
+							Por favor ingrese su correo y contraseña para iniciar sesión.<br />
 								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
 							</p>
 							<div class="form-group">
@@ -80,13 +80,13 @@ exit();
 									<i class="fa fa-lock"></i>
 									 </span>
 									 <a href="forgot-password.php">
-									Forgot Password ?
+									 Has olvidado tu contraseña?
 								</a>
 							</div>
 							<div class="form-actions">
 								
 								<button type="submit" class="btn btn-primary pull-right" name="submit">
-									Login <i class="fa fa-arrow-circle-right"></i>
+									Acceso<i class="fa fa-arrow-circle-right"></i>
 								</button>
 							</div>
 							
@@ -95,7 +95,7 @@ exit();
 					</form>
 
 					<div class="copyright">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase">geoffdeep</span>. <span>todos lo derechos reservados</span>
 					</div>
 			
 				</div>
--- hms/doctor/manage-patient.php
+++ hms/doctor/manage-patient.php
@@ -9,7 +9,7 @@ check_login();
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Doctor | Manage Patients</title>
+		<title>Medico | Administrar pacientes</title>
 		
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -37,14 +37,14 @@ check_login();
 <section id="page-title">
 <div class="row">
 <div class="col-sm-8">
-<h1 class="mainTitle">Doctor | Manage Patients</h1>
+<h1 class="mainTitle">Medico | Administrar pacientes</h1>
 </div>
 <ol class="breadcrumb">
 <li>
-<span>Doctor</span>
+<span>Medico</span>
 </li>
 <li class="active">
-<span>Manage Patients</span>
+<span>Administrar pacientes</span>
 </li>
 </ol>
 </div>
@@ -52,18 +52,18 @@ check_login();
 <div class="container-fluid container-fullw bg-white">
 <div class="row">
 <div class="col-md-12">
-<h5 class="over-title margin-bottom-15">Manage <span class="text-bold">Patients</span></h5>
+<h5 class="over-title margin-bottom-15">Administrar<span class="text-bold">Pacientes</span></h5>
 	
 <table class="table table-hover" id="sample-table-1">
 <thead>
 <tr>
 <th class="center">#</th>
-<th>Patient Name</th>
-<th>Patient Contact Number</th>
-<th>Patient Gender </th>
-<th>Creation Date </th>
-<th>Updation Date </th>
-<th>Action</th>
+<th>Nombre paciente</th>
+<th>Telefono</th>
+<th>Genero</th>
+<th>Fecha creacion</th>
+<th>Fecha actualizacion</th>
+<th>Accion</th>
 </tr>
 </thead>
 <tbody>
