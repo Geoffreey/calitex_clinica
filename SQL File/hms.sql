0dbfd1c386861fbbf16e2debb13a5f7809dee3b3
Geoffreey Cali <geoffrey.torres64@gmail.com>
2023-11-07 07:30:31 -0600

    se modifica nombre de base de datos y credenciales

--- SQL File/hms.sql
+++ SQL File/hms.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 10.4.22-MariaDB : Database - hms
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 10.4.22-MariaDB : Database - hms
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`hms` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`geoflrkf_hms` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-USE `hms`;
+USE `geoflrkf_hms`;
 
 /*Table structure for table `admin` */
 
