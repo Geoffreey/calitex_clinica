d3902d2a1756dab1313a4c2138038c05c9224295
Geoffreey Cali <geoffrey.torres64@gmail.com>
2023-11-06 20:00:36 -0600

    se modifica credenciales de la base de datos para produccion en cpanle

--- hms/admin/include/config.php
+++ hms/admin/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
+define('DB_USER','geoflrkf_geoffreey');
+define('DB_PASS' ,'$%C4l1Torre5');
+define('DB_NAME', 'geoflrkf_hms');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
