f3c050487fc924d19535800166fde0ef88c6f3b4
Geoffreey <geoffrey.torres64@gmail.com>
2023-11-26 09:53:29 -0600

    Se modifican incios de secion para modulos de rayos x y laboratorio

--- hms/tecnico_rx/index.php
+++ hms/tecnico_rx/index.php
@@ -4,16 +4,16 @@ include("include/config.php");
 error_reporting(0);
 if(isset($_POST['submit']))
 {
-$ret=mysqli_query($con,"SELECT * FROM doctors WHERE docEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$ret=mysqli_query($con,"SELECT * FROM tecnico_rx WHERE rxEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
 $num=mysqli_fetch_array($ret);
 if($num>0)
 {
 $extra="dashboard.php";
-$_SESSION['dlogin']=$_POST['username'];
+$_SESSION['rxlogin']=$_POST['username'];
 $_SESSION['id']=$num['id'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=1;
-$log=mysqli_query($con,"insert into doctorslog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['dlogin']."','$uip','$status')");
+$log=mysqli_query($con,"insert into rxlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['rxlogin']."','$uip','$status')");
 $host=$_SERVER['HTTP_HOST'];
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
@@ -22,10 +22,10 @@ exit();
 else
 {
 $host  = $_SERVER['HTTP_HOST'];
-$_SESSION['dlogin']=$_POST['username'];
+$_SESSION['rxlogin']=$_POST['username'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=0;
-mysqli_query($con,"insert into doctorslog(username,userip,status) values('".$_SESSION['dlogin']."','$uip','$status')");
+mysqli_query($con,"insert into rxlog(username,userip,status) values('".$_SESSION['rxlogin']."','$uip','$status')");
 $_SESSION['errmsg']="Nombre de usuario o contraseña no válidos";
 $extra="index.php";
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
@@ -60,7 +60,7 @@ exit();
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../../index.php">	<h2>CaliTex | Acceso Medicos</h2></a>
+				<a href="../../index.php">	<h2>CaliTex | Acceso Rayos X</h2></a>
 				</div>
 
 				<div class="box-login">
