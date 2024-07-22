8f98b0ef0b24b59b2fbb4815126a5893e6eca1e5
Geoffreey Cali <geoffrey.torres64@gmail.com>
2024-07-22 15:56:21 -0600

    Se ordena y depora codigo por div que corrompia interfaz grafica

--- hms/doctor/add-patient.php
+++ hms/doctor/add-patient.php
@@ -46,153 +46,111 @@ if(isset($_POST['submit']))
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 
-	<script>
-function userAvailability() {
-$("#loaderIcon").show();
-jQuery.ajax({
-url: "check_availability.php",
-data:'email='+$("#patemail").val(),
-type: "POST",
-success:function(data){
-$("#user-availability-status1").html(data);
-$("#loaderIcon").hide();
-},
-error:function (){}
-});
-}
-</script>
-	</head>
-	<body>
-		<div id="app">		
-<?php include('include/sidebar.php');?>
-<div class="app-content">
-<?php include('include/header.php');?>
-						
-<div class="main-content" >
-<div class="wrap-content container" id="container">
-						<!-- start: PAGE TITLE -->
-<section id="page-title">
-<div class="row">
-<div class="col-sm-10">
-<h1 class="mainTitle">Paciente | Agregar paciente</h1>
-</div>
-    <ol class="breadcrumb">
-      <li>
-         <span>Paciente</span>
-      </li>
-      <li class="active">
-         <span>Agregar paciente</span>
-      </li>
-    </ol>
-</div>
-</section>
-       <div class="container-fluid container-fullw bg-white">
-          <div class="row">
-            <div class="col-md-11">
-                <div class="row margin-top-30">
-                   <div class="col-lg-8 col-md-11">
-                       <div class="panel panel-white">
-                           <div class="panel-heading">
-                               <h5 class="panel-title">Agregar paciente</h5>
-                            </div>
-                            <div class="panel-body">
-                                <form role="form" name="" method="post">
-
-                                <div class="form-group">
-                                     <label for="doctorname">
-                                        No. Admision
-                                     </label>
-                                     <input type="text" name="patadmi" class="form-control"  placeholder="No. adminision" required="true">
-                                 </div>
-
-                                 <div class="form-group">
-                                     <label for="doctorname">
-                                        Nombre paciente
-                                     </label>
-                                     <input type="text" name="patname" class="form-control"  placeholder="Enter Patient Name" required="true">
-                                 </div>
-                                 <div class="form-group">
-                                    <label for="fess">
-                                       Fecha de nacimiento
-                                    </label>
-                                    <input type="text" name="fena" class="form-control"  placeholder="AAA-MM-DD" required="true" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">
-                                 </div>
-                                 <div class="form-group">
-                                    <label for="fess">
-                                       Telefono
-                                    </label>
-                                    <input type="text" name="patcontact" class="form-control"  placeholder="Enter Patient Contact no" required="true" maxlength="10" pattern="[0-9]+">
-                                 </div>
-                                 <div class="form-group">
-                                    <label for="fess">
-                                        Email
-                                    </label>
-                                    <input type="email" id="patemail" name="patemail" class="form-control"  placeholder="Enter Patient Email id"  onBlur="userAvailability()">
-                                    <span id="user-availability-status1" style="font-size:12px;"></span>
-                                 </div>
-                                 <div class="form-group">
-                                      <label class="block">
-                                       Genero
-                                      </label>
-                                       <div class="clip-radio radio-primary">
-                                          <input type="radio" id="rg-female" name="gender" value="femenino" >
-                                          <label for="rg-female">
-                                              Femenino
-                                          </label>
-                                          <input type="radio" id="rg-male" name="gender" value="masculino">
-                                          <label for="rg-male">
-                                             Masculino
-                                          </label>
-                                      </div>
-                                 </div>
-                                 <div class="form-group">
-                                     <label for="address">
-                                       Direccion
-                                     </label>
-                                     <textarea name="pataddress" class="form-control"  placeholder="Enter Patient Address" required="true"></textarea>
-                                 </div>
-                                 <div class="form-group">
-                                     <label for="fess">
-                                      Edad
-                                     </label>
-                                     <input type="text" name="patage" class="form-control"  placeholder="Enter Patient Age" required="true">
-                                 </div>
-                                 <div class="form-group">
-                                     <label for="fess">
-                                         Historial medico
-                                     </label>
-                                     <textarea type="text" name="medhis" class="form-control"  placeholder="Enter Patient Medical History(if any)" required="true"></textarea>
-                                 </div>	
-
-                                 <button type="submit" name="submit" id="submit" class="btn btn-o btn-primary">
-                                    Agregar
-                                 </button>
-                                </form>
-</div>
-</div>
-</div>
-</div>
-</div>
-<div class="col-lg-12 col-md-12">
-<div class="panel panel-white">
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>				
-</div>
-</div>
-</div>
-			<!-- start: FOOTER -->
-<?php include('include/footer.php');?>
-			<!-- end: FOOTER -->
-		
-			<!-- start: SETTINGS -->
-<?php include('include/setting.php');?>
-			
-			<!-- end: SETTINGS -->
-		</div>
+	    <script>
+           function userAvailability() {
+             $("#loaderIcon").show();
+             jQuery.ajax({
+             url: "check_availability.php",
+             data:'email='+$("#patemail").val(),
+             type: "POST",
+             success:function(data){
+             $("#user-availability-status1").html(data);
+             $("#loaderIcon").hide();
+             },
+             error:function (){}
+            });}
+       </script>
+   </head>
+   <body>
+	 <div id="app">		
+         <?php include('include/sidebar.php');?>
+         <div class="app-content">
+             <?php include('include/header.php');?>
+		     <div class="main-content" >
+                 <div class="wrap-content container" id="container">
+                     <section id="page-title">
+                         <div class="row">
+                             <div class="col-sm-10">
+                                 <h1 class="mainTitle">Paciente | Agregar paciente</h1>
+                             </div>
+                             <ol class="breadcrumb">
+                                 <li>
+                                     <span>Paciente</span>
+                                 </li>
+                                 <li class="active">
+                                     <span>Agregar paciente</span>
+                                 </li>
+                             </ol>
+                          </div>
+                     </section>
+                     <div class="container-fluid container-fullw bg-white">
+                         <div class="row">
+                             <div class="col-md-11">
+                                 <div class="row margin-top-30">
+                                     <div class="col-lg-8 col-md-11">
+                                         <div class="panel panel-white">
+                                             <div class="panel-heading">
+                                                 <h5 class="panel-title">Agregar paciente</h5>
+                                              </div>
+                                              <div class="panel-body">
+                                                 <form role="form" name="" method="post">
+                                                     <div class="form-group">
+                                                         <label for="doctorname">No. Admision</label>
+                                                         <input type="text" name="patadmi" class="form-control"  placeholder="No. adminision" required="true">
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label for="doctorname">Nombre paciente</label>
+                                                         <input type="text" name="patname" class="form-control"  placeholder="Enter Patient Name" required="true">
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label for="fess">Fecha de nacimiento</label>
+                                                         <input type="text" name="fena" class="form-control"  placeholder="AAA-MM-DD" required="true" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label for="fess">Telefono</label>
+                                                         <input type="text" name="patcontact" class="form-control"  placeholder="Enter Patient Contact no" required="true" maxlength="10" pattern="[0-9]+">
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label for="fess">Email</label>
+                                                         <input type="email" id="patemail" name="patemail" class="form-control"  placeholder="Enter Patient Email id"  onBlur="userAvailability()">
+                                                         <span id="user-availability-status1" style="font-size:12px;"></span>
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label class="block">Genero</label>
+                                                         <div class="clip-radio radio-primary">
+                                                             <input type="radio" id="rg-female" name="gender" value="femenino" >
+                                                             <label for="rg-female">Femenino</label>
+                                                             <input type="radio" id="rg-male" name="gender" value="masculino">
+                                                             <label for="rg-male">Masculino</label>
+                                                         </div>
+                                                     </div>
+                                                     <div class="form-group">
+                                                          <label for="address">Direccion</label>
+                                                          <textarea name="pataddress" class="form-control"  placeholder="Enter Patient Address" required="true"></textarea>
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label for="fess">Edad</label>
+                                                         <input type="text" name="patage" class="form-control"  placeholder="Enter Patient Age" required="true">
+                                                     </div>
+                                                     <div class="form-group">
+                                                         <label for="fess">Historial medico</label>
+                                                         <textarea type="text" name="medhis" class="form-control"  placeholder="Enter Patient Medical History(if any)" required="true"></textarea>
+                                                     </div>
+                                                     <button type="submit" name="submit" id="submit" class="btn btn-o btn-primary">Agregar</button>
+                                                 </form>
+                                              </div>
+                                          </div>
+                                     </div>
+                                  </div>
+                              </div>
+                         </div>
+                      </div>
+                 </div>
+             </div>				
+         </div>
+         <?php include('include/footer.php');?>
+         <?php include('include/setting.php');?>
+     </div>
 		<!-- start: MAIN JAVASCRIPTS -->
 		<script src="vendor/jquery/jquery.min.js"></script>
 		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
